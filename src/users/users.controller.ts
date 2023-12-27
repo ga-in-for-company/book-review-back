2dc1776de7639dc2c588931c81e1e22b64b2a0dd
ga-in-for-company <nopainogaini@naver.com>
2023-12-27 23:21:58 +0900

    feat:user프로바이더 생성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { UserInfo } from 'src/common/interface';
 import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UserLoginDto } from 'src/dto/user-login.dto';
+import { VerifyEmailDto } from 'src/dto/verify-emial.dto';
 
 @Controller('users')
 export class UsersController {
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
     console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
   }
 }
